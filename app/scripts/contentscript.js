aa250b16ae8359fb48a1c85806085c45f08b92c2
Amiel Elboim <amielboim@gmail.com>
2016-06-05 00:33:59 +0300

    init base ui

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,4 +1,9 @@
-// bottom menu for every page
-//
 
-var bot_menu = "<div class='sh-wrapper'></div>";
+$('meta').last().after("<link href='http://fonts.googleapis.com/earlyaccess/opensanshebrew.css' rel='stylesheet' type='text/css'>");
+
+// bottom menu for every page
+var bot_menu = "<div id='sh-wrapper'>";
+bot_menu += "<p>שיויתי ה' לנגדי תמיד</p>"
+bot_menu += "</div>";
+
+$('body').append(bot_menu);
